1a83a5446e7a781f9d3f2916bcf98dd7e6aa91c9
Joanne Daudier <jdaudier@outlook.com>
2015-03-07 20:45:42 -0500

    Update server file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var app = require('./api/api');
 var serveStatic = require('serve-static');
+app.set('port', process.env.PORT || 3000);
+app.use(serveStatic('dist', {'index': ['index.html']}));
 
-app.use(serveStatic('dist'));
-app.listen(3000);
+app.listen(app.get('port'));
 
-console.log('Marionette Wires is listening on port 3000');
+console.log('Marionette Wires is listening on port ' + app.get('port'));
