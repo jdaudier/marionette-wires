d144602f3c3b4a5ec3d875c543f5115bf8cf193e
Joanne Daudier <jdaudier@outlook.com>
2015-03-07 21:38:19 -0500

    Move server files around

--- api/api.js
+++ api/api.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
 var api = module.exports = express();
+api.set('port', process.env.PORT || 3000);
+api.use(serveStatic('dist'));
 
 api.use(logger('dev'));
 api.use(bodyParser.json());
@@ -9,4 +12,5 @@ api.use(bodyParser.json());
 require('./books/routes')(api);
 require('./colors/routes')(api);
 
-module.exports = api;
+api.listen(api.get('port'));
+console.log('Marionette Wires is listening on port ' + api.get('port'));
--- app.js
+++
@@ -1,8 +0,0 @@
-var app = require('./api/api');
-var serveStatic = require('serve-static');
-app.set('port', process.env.PORT || 3000);
-app.use(serveStatic('dist', {'index': ['index.html']}));
-
-app.listen(app.get('port'));
-
-console.log('Marionette Wires is listening on port ' + app.get('port'));
